047d7e38e33ae77b661d3e8110de2d376cc9f8ab
adamreaksmey <adaminiature@gmail.com>
2024-03-22 18:50:36 +0700

    update

--- __main__.py
+++ __main__.py
@@ -5,11 +5,16 @@ from functions.exporter import add_export_default
 from functions.defaultWriter import ensure_reWrittenDatas_defined
 from functions.dataMapper import mapper_function
 from functions.sql.generator import insert_data
+from functions.dynamoFormatter import format_dynamodb_json
 
 def runInitProcess():
     input_file = "./sources/data.json"
     output_file = "./logs/content.py"
     output_directory = '.'
+
+    # format to proper dictionary format
+    # The reason this is the same because we are we writing it back to the same file
+    format_dynamodb_json(input_file, input_file)
 
     # Ensure reWrittenDatas is defined in the output file
     ensure_reWrittenDatas_defined(output_file)
---
+++ functions/dynamoFormatter.py
@@ -0,0 +1,29 @@
+import re
+
+def format_dynamodb_json(input_filename, output_filename):
+    try:
+        with open(input_filename, 'r') as file:
+            content = file.read()
+        
+        # Replace all instances of "}\n" (end of a JSON object followed by a newline)
+        # with "},\n" to separate the objects correctly.
+            
+        # This assumes each JSON object is on a single line.
+        formatted_content = re.sub(r'\}\n', '},\n', content)
+        
+        # Remove any trailing comma which may exist after the last JSON object
+        formatted_content = re.sub(r',\n$', '\n', formatted_content)
+        
+        # Enclose the entire content within square brackets to form a valid JSON array
+        final_content = f'[{formatted_content.rstrip(",\n")}]'
+        
+        # Save the formatted content to a new file
+        with open(output_filename, 'w') as file:
+            file.write(final_content)
+        
+        print("File has been successfully formatted and saved.")
+        
+    except FileNotFoundError:
+        print(f"File {input_filename} not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
