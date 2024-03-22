29d9ca7ae59664dd8027cb54728b9fbf8752c8c7
adamreaksmey <adaminiature@gmail.com>
2024-03-22 11:41:13 +0700

    update

--- __main__.py
+++ __main__.py
@@ -9,6 +9,7 @@ from functions.sql.generator import insert_data
 def runInitProcess():
     input_file = "./sources/data.json"
     output_file = "./logs/content.py"
+    output_directory = '.'
 
     # Ensure reWrittenDatas is defined in the output file
     ensure_reWrittenDatas_defined(output_file)
@@ -21,13 +22,11 @@ def runInitProcess():
 
     # Reload the module to reflect changes made by add_export_default
     importlib.reload(content_module)
-    output_directory = '.'  # Adjust as necessary
     mapper_function(content_module.reWrittenDatas, output_directory)
 
     # Generating an sql script
     importlib.reload(content_module)
     q_response = insert_data(content_module.reWrittenDatas)
-    output_directory = '.'  # Adjust this to your specific directory structure
     output_file = os.path.join(output_directory, 'generated_sql/migration_queries.sql')
 
     # Write the SQL queries to a file
