c1170c63cfa19d8dc3fcefaf6846b88c9ddb75ec
adamreaksmey <adaminiature@gmail.com>
2024-03-23 23:07:49 +0700

    formatter

--- functions/dynamo-formatter.mjs
+++ functions/dynamo-formatter.mjs
@@ -1,28 +1,30 @@
-function formatDynamoDBJson(inputFilename, outputFilename) {
-    try {
-        // Read the content from the input file
-        let content = fs.readFileSync(inputFilename, { encoding: 'utf8' });
+const formatDynamoDBJson = (inputFilename, outputFilename) => {
+  try {
+    // Read the content from the input file
+    let content = fs.readFileSync(inputFilename, { encoding: "utf8" });
 
-        // Replace all instances of "}\n" with "},\n" to separate the objects correctly
-        let formattedContent = content.replace(/\}\n/g, '},\n');
+    // Replace all instances of "}\n" with "},\n" to separate the objects correctly
+    let formattedContent = content.replace(/\}\n/g, "},\n");
 
-        // Remove any trailing comma which may exist after the last JSON object
-        formattedContent = formattedContent.replace(/,\n$/, '\n');
+    // Remove any trailing comma which may exist after the last JSON object
+    formattedContent = formattedContent.replace(/,\n$/, "\n");
 
-        // Enclose the entire content within square brackets to form a valid JSON array
-        if (!formattedContent.startsWith('[') || !formattedContent.endsWith(']')) {
-            formattedContent = `[${formattedContent.trim().replace(/,$/, '')}]`;
-        }
-
-        // Save the formatted content to a new file
-        fs.writeFileSync(outputFilename, formattedContent, { encoding: 'utf8' });
-
-        console.log("File has been successfully formatted and saved.");
-    } catch (error) {
-        if (error.code === 'ENOENT') {
-            console.log(`File ${inputFilename} not found.`);
-        } else {
-            console.log(`An error occurred: ${error}`);
-        }
+    // Enclose the entire content within square brackets to form a valid JSON array
+    if (!formattedContent.startsWith("[") || !formattedContent.endsWith("]")) {
+      formattedContent = `[${formattedContent.trim().replace(/,$/, "")}]`;
     }
-}
+
+    // Save the formatted content to a new file
+    fs.writeFileSync(outputFilename, formattedContent, { encoding: "utf8" });
+
+    console.log("File has been successfully formatted and saved.");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      console.log(`File ${inputFilename} not found.`);
+    } else {
+      console.log(`An error occurred: ${error}`);
+    }
+  }
+};
+
+export default formatDynamoDBJson;
