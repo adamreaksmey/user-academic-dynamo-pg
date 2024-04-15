f1106a07638cbf30b841f459199ce89ce3106b9d
adamreaksmey <adaminiature@gmail.com>
2024-04-15 20:19:59 +0700

    update

---
+++ functions/operations/sqlProcessor.mjs
@@ -0,0 +1,54 @@
+import fs from "fs";
+import { sqlToObjects } from "./sqlToObjects.mjs";
+import { randomUUID } from "crypto";
+
+const ibfProdSchoolId = "61f17951-d509-4b60-967b-a84442f949b6";
+const ibfCampusId = "76044dab-2031-4b66-bf0c-be3c273f0687";
+const incrementor = 0;
+import { dobHandlder } from "./data.mjs";
+
+export const processSqlBackup = async (tableName, filePath) => {
+  let formattedContent = [];
+  const sqlFileContent = fs.readFileSync(filePath, { encoding: "utf8" });
+
+  const objectsContent = await sqlToObjects(sqlFileContent).map((data) =>
+    replaceNullWithEmptyString(data)
+  );
+
+
+  if (tableName == "student") {
+    formattedContent = objectsContent.map((data) => {
+      return {
+        tableName,
+        studentId: randomUUID(),
+        idCard: data?.idCard || "",
+        firstName: data?.firstName || "",
+        lastName: data?.lastName || "",
+        firstNameNative: data?.firstName || "",
+        lastNameNative: data?.lastName || "",
+        gender: data?.gender.toLowerCase() || "",
+        dob: dobHandlder(data.dob),
+        remark: [data.remark?.replaceAll("'", "`") ?? ""],
+        status: "start",
+        profile: {
+          position: data?.position?.replaceAll("'", "`") || "",
+          phone: data.phone,
+        },
+        uniqueKey: data?.idCard,
+        campusId: ibfCampusId,
+        
+      };
+    });
+  }
+
+  return formattedContent;
+};
+
+const replaceNullWithEmptyString = (data) => {
+  for (const key in data) {
+    if (data[key] === "NULL") {
+      data[key] = "";
+    }
+  }
+  return data;
+};
--- index.mjs
+++ index.mjs
@@ -9,83 +9,35 @@ import {
   sqlFileOutPutGenerator,
 } from "./functions/sqlGenerator.mjs";
 import { reWriter } from "./functions/re-writer.mjs";
-import formatDynamoDBJson from "./functions/dynamo-formatter.mjs";
-import { sqlToObjects } from "./functions/operations/sqlToObjects.mjs";
+import { processSqlBackup } from "./functions/operations/sqlProcessor.mjs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 /**
- * 
- * @param {*} __filename 
- * @param {*} __dirname 
- * @returns 
- * 
+ *
+ * @param {*} __filename
+ * @param {*} __dirname
+ * @returns
+ *
  *  Note: my sql parser isnt working correctly especially dealing with semi colons
  *  so if found, please console log, it will show you which line has a semi colon and
  *  manually remove them yourself.
  */
 const main = async (__filename, __dirname) => {
-  const sqlFilePath_user = "./generated_sql/lms-service/backup/lms_user_backup.sql";
-  const sqlFileContent_user = fs.readFileSync(sqlFilePath_user, { encoding: "utf8" });
+  let LMS_USERS = [];
+  let LMS_COURSES_USERS = [];
 
-  // console.log(sqlFileContent)
-  const objectsContent_user = await sqlToObjects(sqlFileContent_user);
-  const formattedContent_user = objectsContent_user.map((data) => {
-    return {
-      tableName: "user",
-      ...data,
-    };
-  });
-  fs.writeFileSync(
-    "./logs/backup/users.mjs",
-    `export default ${JSON.stringify(formattedContent_user)}`
+  LMS_USERS = await processSqlBackup(
+    "student",
+    "./generated_sql/lms-service/backup/lms_user_backup.sql"
   );
 
-  const sqlFilePath_courses_user = "./generated_sql/lms-service/backup/lms_courses_users_backup.sql";
-  const sqlFileContent_courses_user = fs.readFileSync(sqlFilePath_courses_user, { encoding: "utf8" });
-
-  const objectsContent_courses_user = await sqlToObjects(sqlFileContent_courses_user);
-  const formattedContent_courses_user = objectsContent_courses_user.map((data) => {
-    return {
-      tableName: "lms_courses_users",
-      ...data,
-    };
-  });
-
-  fs.writeFileSync(
-    "./logs/backup/courses_users.mjs",
-    `export default ${JSON.stringify(formattedContent_courses_user)}`
-  );
-
-  let modulePath = join(__dirname, "./logs/backup/users.mjs");
-  let uniqueUrl = pathToFileURL(modulePath).toString() + "?v=" + Date.now();
-  let jsMapped = await import(uniqueUrl);
-
-  const qResponse = insert_data(jsMapped.default);
-  const outputPath = "./generated_sql/backup-rewritten/user.sql";
+  const qResponse = insert_data(LMS_USERS);
+  const outputPath = "./generated_sql/backup-written/student.sql";
   sqlFileOutPutGenerator(qResponse, __dirname, fs, path, join, outputPath);
 
-  return console.log("Successfully rewritten and mapped user and courses");
-  // Getting file contents
-
-  console.log("-- importing file data --");
-  const data = fs.readFileSync(join(__dirname, "./sources/data.json"), "utf8");
-  const filePath = path.join(__dirname, "./logs/data.mjs");
-  const fileContent = `export default ${data};`;
-
-  reWriter(filePath, fileContent, fs, path);
-
-  // Initial import with unique URL to bypass cache
-  // let modulePath = join(__dirname, "./logs/data.mjs");
-  // let uniqueUrl = pathToFileURL(modulePath).toString() + "?v=" + Date.now();
-  // let jsMapped = await import(uniqueUrl);
-
-  // mapping your json data beforing to the sql generator
-  mapperFunction(jsMapped.default, fs);
-
-  // Import again with a new unique URL to get the updated module
-  console.log("-- re-importing --");
+  return;
 
   /**
    * Guardians
