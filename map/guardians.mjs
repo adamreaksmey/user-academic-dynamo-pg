ebeada45ea4b53fc1f033a07c7902d12a81efec6
adamreaksmey <adaminiature@gmail.com>
2024-04-23 19:45:19 +0700

    update

--- map/guardians.mjs
+++ map/guardians.mjs
@@ -49,24 +49,52 @@ const guardiansToBeReplaced = [
   },
   {
     replacedName: "Vattanac  Bank",
-    toBeUpdatedId: "39262db6-78e0-4e1e-97d5-cdc10264dfb5"
+    toBeUpdatedId: "39262db6-78e0-4e1e-97d5-cdc10264dfb5",
   },
   {
     replacedName: "CAMMA  MICROFINANCE LIMITED",
-    toBeUpdatedId: "b90c50d5-c236-490d-b5dd-8363e9c339be"
+    toBeUpdatedId: "b90c50d5-c236-490d-b5dd-8363e9c339be",
   },
-
-  
-  "Corich  Microfinance PLC.",
-  "FOREIGN  TRADE BANK OF CAMBODIA",
-  "G  B MFI",
-  "NongHyup  Finance",
-  "PANDA  Commercial Bank Plc.",
-  "Paragon  University",
-  "PRINCE  BANK PLC.",
-  "Right  Smart Finance",
-  "Rolya  Plc.",
-  "SaigonHanoi  Bank Cambodia Plc.",
+  {
+    replacedName: "Corich  Microfinance PLC.",
+    toBeUpdatedId: "13b7410c-abb6-4473-9a9b-f451f716977f",
+  },
+  {
+    replacedName: "FOREIGN  TRADE BANK OF CAMBODIA",
+    toBeUpdatedId: "4bb237df-befc-44a9-893e-6c78756f3b4e",
+  },
+  {
+    replacedName: "G  B MFI",
+    toBeUpdatedId: "bae83bce-5564-4c2b-9367-54e296c5417d",
+  },
+  {
+    replacedName: "NongHyup  Finance",
+    toBeUpdatedId: "d3b676e5-a091-4960-9340-712cdb7fdf9a",
+  },
+  {
+    replacedName: "PANDA  Commercial Bank Plc.",
+    toBeUpdatedId: "76e12ba3-5ee0-4862-90ee-9d91b5ae0720",
+  },
+  {
+    replacedName: "Paragon  University",
+    toBeUpdatedId: "b1f9fed0-bb5e-4612-9ad4-6a16d5fef6e6",
+  },
+  {
+    replacedName: "PRINCE  BANK PLC.",
+    toBeUpdatedId: "473edcc2-99f6-438a-be70-0f80b488caa2",
+  },
+  {
+    replacedName: "Right  Smart Finance",
+    toBeUpdatedId: "8dc5ff69-9999-4e6d-b5a4-d02aab87bf8b",
+  },
+  {
+    replacedName: "Rolya  Plc.",
+    toBeUpdatedId: "d4139fd7-989b-41ef-b457-88f420a3d0dd",
+  },
+  {
+    replacedName: "SaigonHanoi  Bank Cambodia Plc.",
+    toBeUpdatedId: "b45a66a3-651d-48c3-afc3-e21605de4a1c",
+  },
 ];
 
 export default guardiansToBeReplaced;
