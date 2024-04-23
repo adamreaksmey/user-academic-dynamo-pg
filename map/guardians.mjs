7a8cb89d409d6dbb6c18f61535a38be4b5b8c2b5
adamreaksmey <adaminiature@gmail.com>
2024-04-23 19:02:29 +0700

    mapped throwaway guardians

--- map/guardians.mjs
+++ map/guardians.mjs
@@ -1,26 +1,68 @@
 const guardiansToBeReplaced = [
-    'AMZ  Microfinance Institution PLc.',
-    'Bangkok  Bank Public Company Limitedm Cambodia Branch',
-    'BANK  FOR INVESTMENT&DEVELOPMENT OF CAMBODIA Plc.',
-    'CAMBODIAN  COMMERCIAL BANK PLC.',
-    'Heng  Feng Commercial Bank',
-    'Hong  Leong Bank (Cambodia) Plc',
-    'Hong  Leong Bank (Cambodia) PLc.',
-    'JC  FINANCE PLC.',
-    'LCH  Microfinance Plc',
-    'Leng  Navatra Capital Plc',
-    'Sathapana  Bank Plc',
-    'Shinhan  Bank (Cambodia) PLc.',
-    'Vattanac  Bank',
-    'CAMMA  MICROFINANCE LIMITED',
-    'Corich  Microfinance PLC.',
-    'FOREIGN  TRADE BANK OF CAMBODIA',
-    'G  B MFI',
-    'NongHyup  Finance',
-    'PANDA  Commercial Bank Plc.',
-    'Paragon  University',
-    'PRINCE  BANK PLC.',
-    'Right  Smart Finance',
-    'Rolya  Plc.',
-    ''
-]
+  {
+    replacedName: "AMZ  Microfinance Institution PLc.",
+    toBeUpdatedId: "51c40d0a-bc9b-4e67-999d-310ed7e3d0d5",
+  },
+  {
+    replacedName: "Bangkok  Bank Public Company Limitedm Cambodia Branch",
+    toBeUpdatedId: "1b7a2bbd-a5e7-4dba-8e36-4d4b507b7973",
+  },
+  {
+    replacedName: "BANK  FOR INVESTMENT&DEVELOPMENT OF CAMBODIA Plc.",
+    toBeUpdatedId: "9da76ee4-639c-4631-9009-54c067e0e680",
+  },
+  {
+    replacedName: "CAMBODIAN  COMMERCIAL BANK PLC.",
+    toBeUpdatedId: "a624e7ea-e7d0-4d46-9190-cb059681879c",
+  },
+  {
+    replacedName: "Heng  Feng Commercial Bank",
+    toBeUpdatedId: "329ae499-3629-4740-b022-59f1c14ee3b0",
+  },
+  {
+    replacedName: "Hong  Leong Bank (Cambodia) Plc",
+    toBeUpdatedId: "7ea9aefb-e26d-4aab-9326-8bedfe7638c8",
+  },
+  {
+    replacedName: "Hong  Leong Bank (Cambodia) PLc.",
+    toBeUpdatedId: "7ea9aefb-e26d-4aab-9326-8bedfe7638c8",
+  },
+  {
+    replacedName: "JC  FINANCE PLC.",
+    toBeUpdatedId: "f426d110-8698-45c0-a143-7f7dc4ceb654",
+  },
+  {
+    replacedName: "LCH  Microfinance Plc",
+    toBeUpdatedId: "e6099289-a8d3-4aee-8287-447abf484223",
+  },
+  {
+    replacedName: "Leng  Navatra Capital Plc",
+    toBeUpdatedId: "e0e75161-3351-41cd-8275-8d484d93bc4c",
+  },
+  {
+    replacedName: "Sathapana  Bank Plc",
+    toBeUpdatedId: "3d3fbab0-d37b-428d-aba5-3d96ed01e0bd",
+  },
+  {
+    replacedName: "Shinhan  Bank (Cambodia) PLc.",
+    toBeUpdatedId: "16df8683-7e2f-4902-8dc0-deff2f646de2",
+  },
+  {
+    replacedName: "Vattanac  Bank",
+    toBeUpdatedId: "39262db6-78e0-4e1e-97d5-cdc10264dfb5"
+  },
+
+  "CAMMA  MICROFINANCE LIMITED",
+  "Corich  Microfinance PLC.",
+  "FOREIGN  TRADE BANK OF CAMBODIA",
+  "G  B MFI",
+  "NongHyup  Finance",
+  "PANDA  Commercial Bank Plc.",
+  "Paragon  University",
+  "PRINCE  BANK PLC.",
+  "Right  Smart Finance",
+  "Rolya  Plc.",
+  "SaigonHanoi  Bank Cambodia Plc.",
+];
+
+export default guardiansToBeReplaced;
