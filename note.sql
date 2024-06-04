1ab046ddede73cd040d6efdf6851dd572b2e7333
adamreaksmey <adaminiature@gmail.com>
2024-06-04 16:34:56 +0700

    update

--- note.sql
+++ note.sql
@@ -7,8 +7,11 @@
 -- USERS
 \copy (SELECT 'INSERT INTO "users" ("firstName", "lastName", "userName", "idCard", "userNumberId") VALUES (''' || "firstName" || ''', ''' || "lastName" || ''', ''' || "userName" || ''', ''' || "idCard" || ''', ''' || "userNumberId" || ''');' FROM "users") TO '/Users/michaellogy/Desktop/sala-projects/dynamo-2-sql-user-academic/input_sql/lms/lms_user_29_05_2024.sql';
 
-
 \copy (SELECT 'INSERT INTO "users" ("firstName", "lastName", "userName", "idCard", "userNumberId") VALUES (''' || "firstName" || ''', ''' || "lastName" || ''', ''' || "userName" || ''', ''' || "idCard" || ''', ''' || "userNumberId" || ''');' FROM "users") TO '/Users/michaellogy/Desktop/sala-projects/dynamo-2-sql-user-academic/input_sql/lms/lms_user_29_05_2024.sql';
+
+
+-- ACADEMIC USERS EXPORT WITH GUARDIAN_ID = '0ab640f1-78f2-47be-87b9-c74a08eb4569'
+\copy (SELECT 'INSERT INTO "guardian_student" ("guardianId", "uniqueKey") VALUES (''' || "guardianId" || ''', ''' || "uniqueKey" || ''');' FROM "guardian_student") TO '/Users/michaellogy/Desktop/sala-projects/dynamo-2-sql-user-academic/input_sql/lms/lms_user_04_06_2024.sql';
 
 -- select count(*)
 -- from "lms_user_progress" where
